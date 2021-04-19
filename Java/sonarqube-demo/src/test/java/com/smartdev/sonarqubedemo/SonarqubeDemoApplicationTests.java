fc2552448393e99b775a0ad5e22480e6b87c1b57
Hieu Tran <tr.kimhieu@gmail.com>
2021-04-19 16:52:47 +0700

    chore: Add Spring Boot project

---
+++ Java/sonarqube-demo/src/test/java/com/smartdev/sonarqubedemo/SonarqubeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdev.sonarqubedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
